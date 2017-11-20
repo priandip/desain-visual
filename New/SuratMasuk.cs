093de97e20743092e6d37d1d325ccee46c0eecd7
dummydanielpardamean <dummydanielpardamean@gmail.com>
2017-11-20 07:01:21 -0800

    upload surat berfungsi

--- New/SuratMasuk.cs
+++ New/SuratMasuk.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace New
 {
@@ -24,8 +25,24 @@ namespace New
             string perihal = Perihal.Text;
             string lampiran = Lampiran.Text;
             string tanggalTerima = TanggalTerima.Value.ToString("yyyy-MM-dd");
+            string uploadDirectory = Path.Combine(Application.StartupPath, "surat");
 
+            if (!Directory.Exists(uploadDirectory)) Directory.CreateDirectory(uploadDirectory);
 
+            string filename = noSurat + Path.GetExtension(lampiran);
+
+            File.Copy(lampiran, Path.Combine(uploadDirectory, filename));
+
+        }
+
+        private void CariBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Lampiran.Text = ofd.FileName;
+            }
         }
     }
 }
